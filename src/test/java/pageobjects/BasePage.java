a329b18d1ce31bfc09b59ba307a94c60fad5416b
QaAutomationEngineer <damagehcmf@gmail.com>
2024-12-19 18:15:17 +0100

    Improve logging

--- src/test/java/pageobjects/BasePage.java
+++ src/test/java/pageobjects/BasePage.java
@@ -6,17 +6,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class BasePage {
     private By loadingIcon = By.cssSelector(".blockUI");
     protected final WebDriver driver;
     protected final String baseURL;
     protected final WebDriverWait wait;
+    protected final Logger log;
 
     protected BasePage(WebDriver driver) {
         this.driver = driver;
         baseURL = new ConfigurationReader().getBaseUrl();
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        this.log = LoggerFactory.getLogger(this.getClass());
     }
 
     protected void waitForLoadingIconDisappear() {
