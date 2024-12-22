1fff6aeaec0d5c67597949cf0bf97860a54c2a47
QaAutomationEngineer <damagehcmf@gmail.com>
2024-12-22 15:38:53 +0100

    ApiBaseTest introduced, AuthenticationApiTests are working fine

--- src/test/java/ApiBaseTest.java
+++ src/test/java/ApiBaseTest.java
@@ -1,5 +1,10 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,19 +13,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ApiBaseTest {
 
-    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
+    private static final Logger log = LoggerFactory.getLogger(ApiBaseTest.class);
+    protected static final String BASE_URI = "http://localhost:3000/api";
 
     @BeforeAll
-    public static void cleanDatabase() {
-        log.info("Cleaning database");
-        given().when().get("http://localhost:3000/api/restoreDB").then().statusCode(201);
-    }
+    public static void setup() {
+        log.info("Starting API test setup");
 
-    @BeforeAll
-    static void apiSetup() {
-        baseURI = "http://localhost:3000";
-        log.info("Setting up API base URI: " + baseURI);
-        filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
-        log.info("Setting up API filters" + filters());
+        // 1. Database cleanup
+        try {
+            given()
+                    .when()
+                    .get(BASE_URI + "/restoreDB")
+                    .then()
+                    .statusCode(201);
+        } catch (Exception e) {
+            log.error("Failed to clean database", e);
+            throw new RuntimeException("Test setup failed - couldn't clean database", e);
+        }
+
+        // 2. Configure filters
+        filters(new RequestLoggingFilter(LogDetail.ALL),
+                new ResponseLoggingFilter(LogDetail.BODY));
+
+        // 3. Configure request specification
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setContentType(ContentType.JSON)
+                .setAccept(ContentType.JSON)
+                .build();
+
+        // 4. Configure response specification
+        responseSpecification = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .build();
+
+        log.info("API test setup completed");
     }
 }
--- src/test/java/AuthenticationApiTests.java
+++ src/test/java/AuthenticationApiTests.java
@@ -1,18 +1,15 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.responseSpecification;
 
-public class AuthenticationApiTests {
+public class AuthenticationApiTests extends ApiBaseTest {
 
+    private static final String LOGIN_ENDPOINT = "/login";
     private static RequestSpecification requestSpecification;
     private String validEmail = "damagehcmf@gmail.com";
     private String validPassword = "dupadupa123";
@@ -20,34 +17,14 @@ public class AuthenticationApiTests {
     private String invalidPassword = "dupadupa1234";
     private String expectedErrorMessage = "Incorrect email or password";
 
-    @BeforeAll
-    public static void setupRequestSpecification() {
-        requestSpecification = new RequestSpecBuilder()
-                .log(LogDetail.ALL)
-                .setBaseUri("http://localhost:3000")
-                .setBasePath("/api/login")
-                .setContentType(ContentType.JSON)
-                .setAccept(ContentType.JSON)
-                .build();
-    }
-
-    @BeforeAll
-    public static void setupResponseSpecification() {
-        responseSpecification = new ResponseSpecBuilder()
-                .log(LogDetail.BODY)
-                .expectContentType(ContentType.JSON)
-                .build();
-    }
-
     @Test
     public void loginApiTestWithValidEmailAndValidPasswordTest() {
         String accessToken = given()
                 .spec(requestSpecification)
                 .body("{\"email\": \"" + validEmail + "\", \"password\": \"" + validPassword + "\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
-                .spec(responseSpecification)
                 .statusCode(200)
                 .extract()
                 .path("access_token");
@@ -58,10 +35,9 @@ public class AuthenticationApiTests {
     @Test
     public void loginApiTestWithInvalidEmailAndInvalidPasswordTest() {
         String responseBody = given()
-                .spec(requestSpecification)
                 .body("{\"email\": \"" + invalidEmail + "\", \"password\": \"" + invalidPassword + "\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
@@ -74,10 +50,9 @@ public class AuthenticationApiTests {
     @Test
     public void loginApiTestWithInvalidEmailAndValidPasswordTest() {
         String responseBody = given()
-                .spec(requestSpecification)
                 .body("{\"email\": \"" + invalidEmail + "\", \"password\": \"" + validPassword + "\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
@@ -90,10 +65,9 @@ public class AuthenticationApiTests {
     @Test
     public void loginApiTestWithValidEmailAndInvalidPasswordTest() {
         String responseBody = given()
-                .spec(requestSpecification)
                 .body("{\"email\": \"" + validEmail + "\", \"password\": \"" + invalidPassword + "\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
@@ -106,10 +80,9 @@ public class AuthenticationApiTests {
     @Test
     public void loginApiTestWithEmptyCredentialsTest() {
         String responseBody = given()
-                .spec(requestSpecification)
                 .body("{\"email\": \"\", \"password\": \"\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
@@ -122,10 +95,9 @@ public class AuthenticationApiTests {
     @Test
     public void loginApiTestWithMissingFieldsTest() {
         String responseBody = given()
-                .spec(requestSpecification)
                 .body("{\"email\": \"" + validEmail + "\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
@@ -138,10 +110,9 @@ public class AuthenticationApiTests {
     @Test
     public void loginApiTestWithSpecialCharactersTest() {
         String responseBody = given()
-                .spec(requestSpecification)
                 .body("{\"email\": \"test@#$%^&*()@test.com\", \"password\": \"pass@#$%^&*()\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
@@ -155,10 +126,9 @@ public class AuthenticationApiTests {
     public void loginApiTestWithVeryLongCredentialsTest() {
         String longString = "a".repeat(257);
         String responseBody = given()
-                .spec(requestSpecification)
                 .body("{\"email\": \"" + longString + "@test.com\", \"password\": \"" + longString + "\"}")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
@@ -171,11 +141,10 @@ public class AuthenticationApiTests {
     @Test
     public void loginApiTestWithWrongContentTypeTest() {
         String responseBody = given()
-                .spec(requestSpecification)
                 .contentType(ContentType.XML)
                 .body("<login><email>" + validEmail + "</email><password>" + validPassword + "</password></login>")
                 .when()
-                .post()
+                .post(LOGIN_ENDPOINT)
                 .then()
                 .spec(responseSpecification)
                 .statusCode(401)
