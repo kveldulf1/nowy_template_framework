400c3b0da64acd4d11b0b4ce99ecb36ca62ed845
QaAutomationEngineer <damagehcmf@gmail.com>
2024-12-29 15:20:21 +0100

    Add two tests classses for parallel tests

---
+++ src/test/java/ParallelLoginTests.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import pageobjects.MainPage;
+import pageobjects.RegisterPage;
+import pageobjects.WelcomePage;
+import org.junit.jupiter.api.Assertions;
+import utils.CommonApiCalls;
+
+public class ParallelLoginTests extends BaseTest {
+
+        @Test
+        public void userLogsIn() {
+                WelcomePage welcomePage = new MainPage(driver)
+                                .go()
+                                .headerComponent
+                                .hoverMouseOverUserIcon()
+                                .clickLoginButton()
+                                .loginAsRandomExistingUser();
+
+                Assertions.assertTrue(welcomePage.isUrlCorrect(),
+                                "Url is not correct after login. Current url: " + driver.getCurrentUrl());
+        }
+
+        @Test
+        public void userLogsInWithInvalidUser() {
+                String alertText = new MainPage(driver)
+                                .go()
+                                .headerComponent
+                                .hoverMouseOverUserIcon()
+                                .clickLoginButton()
+                                .loginAsInvalidUser()
+                                .getAlertText();
+
+                Assertions.assertTrue(alertText.contains("Invalid username or password"),
+                                "Alert text does not contain 'Invalid email or password'");
+        
+        }
+
+        @Test
+        public void loggedInUserIsOnTheRightPage() {
+                new CommonApiCalls().goToWelcomePageAsLoggedInUser(driver);
+
+                Assertions.assertTrue(new WelcomePage(driver).isUrlCorrect(),
+                                "Url is not correct after login.");
+        }
+}
